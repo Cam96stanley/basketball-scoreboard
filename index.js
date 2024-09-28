93615c89f78801420fe6aa3a346425c21dd81004
cam96stanley <cam96stanley@gmail.com>
2024-09-28 06:05:53 -0400

    Created js variables
    Added the JS variable to target the dom elements.

---
+++ index.js
@@ -0,0 +1,10 @@
+const homeScoreDisplay = document.getElementById("home-score");
+const guestScoreDisplay = document.getElementById("guest-score");
+
+const addOneHome = document.getElementById("homebtn-add1");
+const addTwoHome = document.getElementById("homebtn-add2");
+const addThreeHome = document.getElementById("homebtn-add3");
+
+const addOneGuest = document.getElementById("guestbtn-add1");
+const addTwoGuest = document.getElementById("guestbtn-add2");
+const addThreeGuest = document.getElementById("guestbtn-add3");
